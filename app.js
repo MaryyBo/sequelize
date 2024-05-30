caa6d13d12d087f2daa54e46852c94a58a44f3ad
MaryyBo <marusya.bodrug@gmail.com>
2024-05-30 15:27:16 +0200

    Cтатичні файли

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const router = require ('./routes/index')
-const {errorHandler} = require('./errorHandler')
+const router = require('./routes/index')
+const { errorHandler } = require('./errorHandler')
+const { STATIC_PATH } = require('./config/path.config');
 
 const app = express();
 
@@ -8,6 +9,8 @@ const bodyParser = express.json();
 app.use(bodyParser);
 
 // або скорочено = app.use(express.json());
+
+app.use(express.static(STATIC_PATH));
 
 app.use('/api', router);
 
